9ec217a368b9aa2cf821300c544e15cbf09fa487
Dominique Vilain <dominique.vilain@hepl.be>
2021-02-10 12:01:30 +0100

    add Brian Suda Example

---
+++ n5-animation-svg/n5-05-animated-drawing-Brian-Suda/script.js
@@ -0,0 +1,6 @@
+/**
+  Une contrainte particulière est à respecter ici :
+  Au lieu de faire l’animation à l’aide de CSS, je vous demande de la réaliser à l’aide de JS. 
+
+  Vous avez besoin pour cela d’appeler de façon récurrente, 60 fois par secondes, une fonction de dessin qui se contente de changer la valeur de l’offset. Pour appeler cette fonction, vous utilisere requestAnimationFrame, qui sera une fonction souvent utilisée dans la suite du cours.
+**/
